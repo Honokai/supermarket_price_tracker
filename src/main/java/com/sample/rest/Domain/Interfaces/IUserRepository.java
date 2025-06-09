1088a6e5b9d4f0df20068a390d7b3b5e240d9de7
Honokai <emerson_hatred@hotmail.com>
2025-06-09 16:23:58 -0300

    enable search by user email

--- src/main/java/com/sample/rest/Domain/Interfaces/IUserRepository.java
+++ src/main/java/com/sample/rest/Domain/Interfaces/IUserRepository.java
@@ -1,9 +1,11 @@
 package com.sample.rest.Domain.Interfaces;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sample.rest.Domain.Entities.User;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByEmail(String email);
 }
