6e71570fa259477b09d07d599aa076a4c73240d4
Honokai <emerson_hatred@hotmail.com>
2025-06-09 17:21:50 -0300

    removing unused imports

--- src/main/java/com/sample/rest/Domain/Entities/User.java
+++ src/main/java/com/sample/rest/Domain/Entities/User.java
@@ -1,12 +1,5 @@
 package com.sample.rest.Domain.Entities;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.CredentialsContainer;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
